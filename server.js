a8d9e03bc0f0e8e042431d492659c15107135961
Prashant Tripathi <90204816+ptripathi2002@users.noreply.github.com>
2023-03-28 20:06:18 +0530

    Update server.js

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(bodyParser.json());
 
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: ["http://localhost:3000", "https://cipherschools.vercel.app"],
     credentials: true,
   })
 );
